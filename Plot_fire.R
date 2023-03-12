ba786070a63fab12d96a6945131afbab23d16f40
EduardoArle <eduardo.arle@idiv.de>
2023-03-12 10:59:45 +0200

    20230312

--- Plot_fire.R
+++ Plot_fire.R
@@ -15,6 +15,7 @@ table <- read.csv('trait_df.csv')
 
 #include fireseason info into shp attribute table
 shp_grid$fireseason <- NA
+shp_grid$phylacine <- NA
 
 for(i in 1:nrow(shp_grid))
 {
@@ -22,6 +23,7 @@ for(i in 1:nrow(shp_grid))
   
   if(length(a) > 0){
     shp_grid$fireseason[i] <- table$fireseason[a]
+    shp_grid$phylacine[i] <- table$phylacine[a]
   }
   
   print(i)
@@ -39,43 +41,43 @@ worldmapframe <- readRDS("Worldmapframe.rds")
 worldmapframe <- spTransform(worldmapframe,CRS(proj4string(world)))
 shp2 <- spTransform(shp_grid,CRS(proj4string(world)))
 
-#create colour ramp to represent the values
-colramp <- colorRampPalette(c("#9e0142", "#d53e4f", "#f46d43",
-                              "#fdae61", "#fee08b", "#ffffbf",
-                              "#e6f598", "#abdda4", "#66c2a5",
-                              "#3288bd", "#5e4fa2"))
-                                       
-
-#populate the table with the colours to be plotted 
-shp2$colours_fire <- colramp(100)[cut(c(0,9,shp_grid$fireseason), 
-                                 breaks = 100)][-c(1,2)]
-
-shp2$colours_fire[which(is.na(shp2$colours_fire))] <- 'gray80'
-
-### PLOT
-par(mar=c(2,2,2,2), bg = 'white')
-plot(shp2, border = NA , col = shp2$colours_fire)
-plot(worldmapframe , add = T)
-
-#plot icons legend (first load function)
-myGradientLegend(valRange = c(min(shp_grid$fireseason, na.rm = T), 
-                              max(shp_grid$fireseason, na.rm = T)),
-                 pos=c(0.3,0.1,0.8,0.12),
-                 color = colramp(100),
-                 side = 1,
-                 n.seg = 0,
-                 values = c(min(shp_grid$fireseason, na.rm = T), 
-                            max(shp_grid$fireseason, na.rm = T)),
-                 cex = 2)
-
-myGradientLegend(valRange = c(min(shp_grid$fireseason, na.rm = T), 
-                              max(shp_grid$fireseason, na.rm = T)),
-                 pos=c(0.21,0.1,0.225,0.12),
-                 color = 'gray80',
-                 side = 1,
-                 n.seg = 0,
-                 values = c(NA,'NA'),
-                 cex = 2)
+# #create colour ramp to represent the values
+# colramp <- colorRampPalette(c("#9e0142", "#d53e4f", "#f46d43",
+#                               "#fdae61", "#fee08b", "#ffffbf",
+#                               "#e6f598", "#abdda4", "#66c2a5",
+#                               "#3288bd", "#5e4fa2"))
+#                                        
+# 
+# #populate the table with the colours to be plotted 
+# shp2$colours_fire <- colramp(100)[cut(c(0,9,shp_grid$fireseason), 
+#                                  breaks = 100)][-c(1,2)]
+# 
+# shp2$colours_fire[which(is.na(shp2$colours_fire))] <- 'gray80'
+# 
+# ### PLOT
+# par(mar=c(2,2,2,2), bg = 'white')
+# plot(shp2, border = NA , col = shp2$colours_fire)
+# plot(worldmapframe , add = T)
+# 
+# #plot icons legend (first load function)
+# myGradientLegend(valRange = c(min(shp_grid$fireseason, na.rm = T), 
+#                               max(shp_grid$fireseason, na.rm = T)),
+#                  pos=c(0.3,0.1,0.8,0.12),
+#                  color = colramp(100),
+#                  side = 1,
+#                  n.seg = 0,
+#                  values = c(min(shp_grid$fireseason, na.rm = T), 
+#                             max(shp_grid$fireseason, na.rm = T)),
+#                  cex = 2)
+# 
+# myGradientLegend(valRange = c(min(shp_grid$fireseason, na.rm = T), 
+#                               max(shp_grid$fireseason, na.rm = T)),
+#                  pos=c(0.21,0.1,0.225,0.12),
+#                  color = 'gray80',
+#                  side = 1,
+#                  n.seg = 0,
+#                  values = c(NA,'NA'),
+#                  cex = 2)
 
 
 #### alternative
@@ -100,7 +102,7 @@ plot(worldmapframe , add = T)
 #plot icons legend (first load function)
 myGradientLegend(valRange = c(min(shp_grid$fireseason, na.rm = T), 
                               max(shp_grid$fireseason, na.rm = T)),
-                 pos=c(0.3,0.1,0.8,0.12),
+                 pos=c(0.27,0.1,0.68,0.12),
                  color = colramp(100),
                  side = 1,
                  n.seg = 0,
@@ -110,9 +112,47 @@ myGradientLegend(valRange = c(min(shp_grid$fireseason, na.rm = T),
 
 myGradientLegend(valRange = c(min(shp_grid$fireseason, na.rm = T), 
                               max(shp_grid$fireseason, na.rm = T)),
-                 pos=c(0.21,0.1,0.225,0.12),
+                 pos=c(0.73,0.1,0.743,0.12),
                  color = 'gray80',
                  side = 1,
                  n.seg = 0,
                  values = c(NA,'NA'),
                  cex = 2)
+
+
+#plot animal presence
+
+#populate the table with the colours to be plotted 
+shp2$colours_phylacine <- colramp(100)[cut(c(min(shp_grid$phylacine, na.rm =T),
+                                             max(shp_grid$phylacine, na.rm =T),
+                                             shp_grid$phylacine), 
+                                      breaks = 100)][-c(1,2)]
+
+
+shp2$colours_phylacine[which(is.na(shp2$colours_phylacine ))] <- 'gray80'
+  
+### PLOT
+par(mar=c(2,2,2,2), bg = 'white')
+plot(shp2, border = NA , col = shp2$colours_phylacine)
+plot(worldmapframe , add = T)
+
+#plot icons legend (first load function)
+myGradientLegend(valRange = c(min(shp_grid$phylacine, na.rm = T), 
+                              max(shp_grid$phylacine, na.rm = T)),
+                 pos=c(0.27,0.1,0.68,0.12),
+                 color = colramp(100),
+                 side = 1,
+                 n.seg = 0,
+                 values = c(min(shp_grid$phylacine, na.rm = T), 
+                            max(shp_grid$phylacine, na.rm = T)),
+                 cex = 2)
+
+myGradientLegend(valRange = c(min(shp_grid$phylacine, na.rm = T), 
+                              max(shp_grid$phylacine, na.rm = T)),
+                 pos=c(0.73,0.1,0.743,0.12),
+                 color = 'gray80',
+                 side = 1,
+                 n.seg = 0,
+                 values = c(NA,'NA'),
+                 cex = 2)
+
